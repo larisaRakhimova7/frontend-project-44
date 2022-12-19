23b8d4fce424d3a811c420cbefe182ce772ff513
larisaRakhimova7 <staffinik@gmail.com>
2022-12-19 15:00:41 +0300

    step4

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,3 @@
 #!/usr/bin/env node
 
-import {userName} from '../src/cli.js';
-
+import userName from '../src/cli.js';
--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,6 @@
 import readlineSync from 'readline-sync';
-console.log("Welcome to the Brain Games!");
-export const userName = readlineSync.question('May I have your name?');
+
+console.log('Welcome to the Brain Games!');
+const userName = readlineSync.question('May I have your name?');
+export default userName;
 console.log(`Hello, ${userName}!`);
